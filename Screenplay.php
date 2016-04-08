619c463475ca077ab16f45f8f02d1d06692b24db
Isarra <zhorishna@gmail.com>
2016-04-08 15:01:44 +0000

    Fix error when loading old-style due to no i18n.alias whatever
    Also tweaked css
    
    Change-Id: Id5f0d191835e302cc5a1a3803eff748b7bf829eb

--- Screenplay.php
+++ Screenplay.php
@@ -3,7 +3,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Screenplay' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Screenplay'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['ScreenplayAlias'] = __DIR__ . '/Screenplay.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for Screenplay extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
