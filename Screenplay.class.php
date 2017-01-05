b32feb1eb2e33866c72817d5a15fafb11832f88c
Isarra <zhorishna@gmail.com>
2017-01-05 06:13:14 +0000

    Add support for having miscellaneous html tags outside of blocks
    Mostly intended for wrapping multiple lines in divs. Also allows other
    arbitrary html as long as it contains no text (text should be parsed as
    actual lines).
    
    What html will render remains up to the mw parser settings.
    
    Change-Id: I2802254b6bce28d61851c1d27fd39281a5ab80d4

--- Screenplay.class.php
+++ Screenplay.class.php
@@ -18,6 +18,8 @@ class Screenplay {
 	 * @return string
 	 */
 	public static function render( $input, array $args, Parser $parser, PPFrame $frame ) {
+		// TODO: expand templates *first* so they can be parsed as directly inserted screenplay source if needed
+
 		// Start by removing all trailing whitespace on each line, as it makes further regex processing
 		// unpleasant. Keep leading whitespace, which might sometimes be intentional. The list of
 		// characters to remove is taken from trim()'s documentation, without '\n'.
@@ -44,12 +46,21 @@ class Screenplay {
 				return str_repeat( '<br />', intval( $matches[1] ) );
 			}
 
+			// Skip html tags that contain no content
+			$doc = new DOMDocument;
+			MediaWiki\suppressWarnings();
+			$doc->loadHTML( $block );
+			MediaWiki\restoreWarnings();
+			if ( Screenplay::isHtmlTags( $doc->documentElement ) ) {
+				return $block;
+			}
+
 			// 'shot-heading': a single line where the first four letters are 'INT.' or 'EXT.'
 			if ( preg_match( '/^(?:INT[., -]|EXT[., -]).+$/', $block ) ) {
 				return
 					'<div class="sp-slug sp-shot-heading">' .
 						"\n" .
-						$parser->recursiveTagParse( $block, $frame ) .
+						$block .
 					'</div>';
 			}
 
@@ -68,13 +79,13 @@ class Screenplay {
 						return
 							'<div class="sp-paren">' .
 								"\n" .
-								$parser->recursiveTagParse( $line, $frame ) .
+								$line .
 							'</div>';
 					} else {
 						return
 							'<div class="sp-dialogue">' .
 								"\n" .
-								$parser->recursiveTagParse( $line, $frame ) .
+								$line .
 							'</div>';
 					}
 				}, $lines );
@@ -83,7 +94,7 @@ class Screenplay {
 					'<div class="sp-line">' .
 						'<div class="sp-speaker">' .
 							"\n" .
-							$parser->recursiveTagParse( $speaker, $frame ) .
+							$speaker .
 						'</div>' .
 						implode( '', $lines ) .
 					'</div>';
@@ -93,16 +104,33 @@ class Screenplay {
 			return
 				'<div class="sp-slug">' .
 					"\n" .
-					$parser->recursiveTagParse( $block, $frame ) .
+					$block .
 				'</div>';
 		}, $blocks );
 
 		$parser->getOutput()->addModuleStyles( 'ext.screenplay' );
 
 		return
-			'<div class="screenplay">' .
+			$parser->recursiveTagParse( '<div class="screenplay">' .
 				"\n" .
 				implode( '', $blocks ) .
-			'</div>';
+			'</div>', $frame );
+	}
+
+	/** Helper function for render to check if block contains html text nodes, or is just tags
+	 * @param mixed element to check
+	 * @return bool
+	 */
+	private static function isHtmlTags( $element ) {
+		if ( !is_object( $element ) || $element->nodeType == XML_TEXT_NODE ) {
+			return false;
+		}
+		for( $i = 0; $i < $element->childNodes->length; $i++ ) {
+			if ( !Screenplay::isHtmlTags( $element->childNodes->item( $i ) ) ) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
